92efb72e85dd52e2508f55ca41aaa835655f4de7
Marat Mukhammed <m.mukhammed@alantech.kz>
2025-06-29 10:57:46 +0500

    fix: ci/cd

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+	// Config options...
+	// Disable SSR for static export to GitHub Pages
+	ssr: false,
 } satisfies Config;
